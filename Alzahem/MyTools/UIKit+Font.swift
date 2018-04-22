29945e6bc0cbcd396c86232572694cb575ad24b2
Ibrahim Samak <ibrahim.samak@247labs.com>
2018-04-22 23:37:08 +0300

    Document base font helper

--- Alzahem/MyTools/UIKit+Font.swift
+++ Alzahem/MyTools/UIKit+Font.swift
@@ -4,7 +4,8 @@ import UIKit
 
 
 extension UIFont {
-    
+
+    /// Name of the app's base font (used for Arabic UI text).
     static func getFontBaseLang()->String{
         return "GEDinarOne-Medium"
     }
