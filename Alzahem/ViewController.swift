21ea3012f01cea499f9b0b594d62d4bc32c9f755
Ibrahim M. Samak <ibrahimm.samak@ibrahims-macbook-pro.local>
2018-07-11 15:51:52 +0300

    Initial Commit

---
+++ Alzahem/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Alzahem
+//
+//  Created by ibrahim M. samak on 7/11/18.
+//  Copyright © 2018 ibrahim M. samak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
