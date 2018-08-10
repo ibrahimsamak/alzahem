df6d0842a98141cbc73a5675fd49fec9bf01d410
Ibrahim Samak <ibrahim.samak@247labs.com>
2018-08-10 03:36:53 +0300

    Document root navigation controller

--- Alzahem/rootNavigationViewController.swift
+++ Alzahem/rootNavigationViewController.swift
@@ -8,13 +8,17 @@
 
 import UIKit
 
+/// Root navigation controller for the main (post-login) flow. On load it stores
+/// itself on the `AppDelegate` (`mainRootNav`) so other code can push onto the
+/// primary navigation stack.
 class rootNavigationViewController: UINavigationController {
-    
+
     override func viewDidLoad()
     {
         super.viewDidLoad()
         if #available(iOS 10.0, *)
         {
+            // Expose this navigation controller to the app delegate.
             let appDelegate = UIApplication.shared.delegate as! AppDelegate
             appDelegate.mainRootNav = self
         }
