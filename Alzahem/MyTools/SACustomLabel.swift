bda46d776b432a871306d2212dd6bd7a5c61a19d
Ibrahim Samak <ibrahim.samak@247labs.com>
2018-08-22 03:10:48 +0300

    Document language-aware custom label

--- Alzahem/MyTools/SACustomLabel.swift
+++ Alzahem/MyTools/SACustomLabel.swift
@@ -8,9 +8,12 @@
 
 import UIKit
 
+/// Label that chooses its font by language: `ar_font` for Arabic, `en_font`
+/// otherwise, both configurable from Interface Builder.
 class SACustomLabel: UILabel
 {
-    
+
+    /// Font size used when the language is Arabic.
     @IBInspectable
     public var ar_font: CGFloat = 17 {
         didSet {
