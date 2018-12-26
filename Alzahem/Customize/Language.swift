321a2f7af2f25fb0b11cb2a010c27ac234382c70
Ibrahim Samak <ibrahim.samak@247labs.com>
2018-12-26 07:49:28 +0300

    Document language default accessor

--- Alzahem/Customize/Language.swift
+++ Alzahem/Customize/Language.swift
@@ -8,17 +8,21 @@
 
 import Foundation
 
+/// Thin wrapper over the `AppleLanguages` user-default used to read and set the
+/// app's active language code (e.g. "ar", "en").
 class Language{
-    
+
+    /// The first entry of `AppleLanguages` — the language the app runs in.
     class func currentLanguage() -> String{
-        
+
         let ns = UserDefaults.standard
         let langs = ns.value(forKey: "AppleLanguages") as! NSArray
         let firstLang = langs.firstObject as! String
-        
+
         return firstLang
     }
-    
+
+    /// Makes `lang` the preferred language for the next launch.
     class func setAppLanguage(lang:String){
         
         let ns = UserDefaults.standard
